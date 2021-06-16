3571949b3cc8f9b6ef98c0a99f351ba1cd1008cd
qinjiabo1990 <qinjiabo1990@gmail.com>
2021-06-16 12:51:42 +1000

    add bob and bobpage

---
+++ src/components/Bob/Bob.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Bob.module.css';
+
+const Bob = () => {
+    return (
+        <>
+            <div className={classes.title}>
+                <h1>Hello, I am Bob</h1>
+            </div>
+        </>
+    )
+};
+
+export default Bob;
