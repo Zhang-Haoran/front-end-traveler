81a6de6470b0907072c9ad62f3ab1594da3464f5
KyrieeWen <245032074@qq.com>
2021-06-16 11:21:20 +1000

    Testing

--- src/components/Kyrie/components/Kyrie.jsx
+++ src/components/Kyrie/components/Kyrie.jsx
@@ -1,5 +1,5 @@
 import { Title } from "../styles";
 const Kyrie = () => {
-  return <Title>Hello from Kyrie Wen</Title>;
+  return <Title>Hello from Kyrie Wen and this is a test for sourceTree</Title>;
 };
 export default Kyrie;
